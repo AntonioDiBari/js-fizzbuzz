620f7e8ce402c683c19b30522882c9f508f7ae0a
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-11 16:40:21 +0100

    feat: comandi con il DOM

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 let mex;
+const container = document.getElementById("container-card");
 
 for (let i = 1; i <= 100; i++) {
   if (i % 5 == 0 && i % 3 == 0) {
     mex = "FizzBuzz";
-    console.log(mex);
   } else if (i % 5 == 0) {
     mex = "Buzz";
-    console.log(mex);
   } else if (i % 3 == 0) {
     mex = "Fizz";
-    console.log(mex);
   } else {
-    console.log(i);
+    mex = i;
   }
+  console.log(mex);
+  container.innerHTML += `
+  <div class="card ${mex}">
+  <div class="box">
+${mex}</box>
+  </div>`;
 }
