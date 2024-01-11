38f9d926bb6d4304beb6a4b2e554e44195022bb9
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-11 14:52:25 +0100

    feat: ciclo for con messaggi diversi per i multipli

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("ok JS");
+let mex;
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 == 0 && i % 3 == 0) {
+    mex = "FizzBuzz";
+    console.log(mex);
+  } else if (i % 5 == 0) {
+    mex = "Buzz";
+    console.log(mex);
+  } else if (i % 3 == 0) {
+    mex = "Fizz";
+    console.log(mex);
+  } else {
+    console.log(i);
+  }
+}
